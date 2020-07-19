806b8be5ac0afb4240cf9fd67392a6a0ab1edaed
Konstantin Tsypnyatov <cipoch@gmail.com>
2020-07-19 14:23:16 -0400

    ляляля
    25

---
+++ src/com/syntax/class25/FinalOverload.java
@@ -0,0 +1,39 @@
+package com.syntax.class25;
+
+public class FinalOverload {
+
+	
+	public final static void doSomething(String o) {
+        System.out.println("String");
+    }
+
+	
+	
+	
+	
+	public static void main(String[] args) {
+		
+		doSomething("dd");
+		
+		
+	    Base b = new Base();
+	    Base d = new Derived();
+	    b.doSomething(3);
+	    d.doSomething(5);
+	}
+	
+	
+}
+
+
+class Base {
+	public final void doSomething(int o) {
+        System.out.println("int");
+    }
+}
+
+class Derived extends Base {
+    public void doSomething(double i) {
+        System.out.println("Int");
+    }
+}
